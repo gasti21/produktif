22f3a7168261475f827638742aa90c9f1435315f
gasti21 <124746034+gasti21@users.noreply.github.com>
2023-09-14 13:30:18 +0700

    Add files via upload

---
+++ operator 1.php
@@ -0,0 +1,12 @@
+<?php
+    $tugas1 = 90 ;
+    $tugas2 = 80 ;
+
+    $jumlah = $tugas1 + $tugas2 ;
+    $rerata = $jumlah / 2 ;
+
+        echo "Nilai tugas 1 : " . $tugas1 . "<br> " ;
+        echo "Nilai tugas 2 : " . $tugas2 . "<br> " ;
+        echo "Jumlah Tugas : " . $jumlah . "<br> " ;
+        echo "Rerata Tuga : " . $rerata . "<br> " ;
+        
